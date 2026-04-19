9443fcbdac32177d45ff94f31c8abe77dd7a8b08
Asfaq <asfaqfahad@gmail.com>
2026-04-19 23:27:09 +0600

    updated

--- FINAL Term/Lab 1 php/Controller/Registrationvalidation.php
+++ FINAL Term/Lab 1 php/Controller/Registrationvalidation.php
@@ -12,5 +12,20 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
         $name = $_REQUEST["name"];
         $password= $_REQUEST["password"];
 
+    if(!empty($name) && strlen($name)>=5)
+        {
+             echo "User Name: ".$name;
+        }
+    else{
+            echo "UserName must be greater than 5 char";
+        }
+    if(strlen($password)>4)
+        {
+            $validpassword = $password;
+            echo "Password: ".$validpassword;
+        }
+    else{   
+         $validpassword = "Password Must be 4 Digit minimum";               
+        }
     }
 ?>
