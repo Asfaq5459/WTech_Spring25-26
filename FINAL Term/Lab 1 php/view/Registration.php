592f09c75eaac0b978b42c2c22ec1aa946884d24
Asfaq <asfaqfahad@gmail.com>
2026-04-19 17:53:33 +0600

    updated

---
+++ FINAL Term/Lab 1 php/view/Registration.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Registration Log In Form</title>
+    </head>
+
+    <body>
+        <form method ="post" action="">
+            <table>
+                <tr>
+                     <td><p style = 'color: red '> * Required Field </p></td><br>
+
+            </table>
+        </form>
+    </body>
+</html>
