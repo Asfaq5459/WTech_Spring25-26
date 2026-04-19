c98153525a03c93b906c6a5f6225ff7b03b80985
Asfaq <asfaqfahad@gmail.com>
2026-04-19 18:58:02 +0600

    run

--- FINAL Term/Lab 1 php/view/Registration.php
+++ FINAL Term/Lab 1 php/view/Registration.php
@@ -9,7 +9,12 @@
             <table>
                 <tr>
                      <td><p style = 'color: red '> * Required Field </p></td><br>
-
+                </tr>
+                <tr>
+                    <td> <label for ="UserName"> User Name: </label></td>
+                    <td> <input type ="text" id = "name" name = "name"> <?php echo $name ?></td>
+                    <td> <p style = 'color: red'>*</p> </td>
+                </tr>
             </table>
         </form>
     </body>
