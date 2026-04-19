0cfa7c164ffd467c1a3850aedd14520324f7c467
Asfaq <asfaqfahad@gmail.com>
2026-04-19 22:51:37 +0600

    update

--- FINAL Term/Lab 1 php/view/Registration.php
+++ FINAL Term/Lab 1 php/view/Registration.php
@@ -14,6 +14,14 @@
                     <td> <label for ="UserName"> User Name: </label></td>
                     <td> <input type ="text" id = "name" name = "name"> <?php echo $name ?></td>
                     <td> <p style = 'color: red'>*</p> </td>
+                
+                </tr>
+               <tr>
+                    <td> <label for ="password"> Password: </label></td>
+                    <td> <input type = "password" id="pass" name ="password"><?php echo $password ?></td>
+                </tr>
+                <tr>
+                    <td><input type = "submit" id = "submit" name = "submit" </td>
                 </tr>
             </table>
         </form>
