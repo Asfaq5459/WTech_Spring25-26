ee343bd8445d46f3b1ad3203a732ab90e8d8f1b4
Asfaq <asfaqfahad@gmail.com>
2026-04-20 14:25:44 +0600

    updated

--- FINAL Term/T_SessionCokkie/Controller/Registratiovalidation.php
+++ FINAL Term/T_SessionCokkie/Controller/Registratiovalidation.php
@@ -2,16 +2,100 @@
 
 session_start();
 
-$name ="";
-$password="";
+$name="";
+$password ="";
 $email = "";
 $website = "";
 $comment = "";
 $gender = "";
 
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+    $name = $_POST["name"] ?? "";
+    $password = $_POST["password"] ?? "";
+    $email = $_POST["email"] ?? "";
+    $website = $_POST["website"] ?? "";
+    $comment = $_POST["comment"] ?? "";
+    $gender = $_POST["gender"] ?? "";
 
+     if(!empty($name) && strlen($name)>=5)
+    {
+        echo "User Name: ".$name."<br>";
+    }
+    else
+    {
+        echo "UserName must be greater than 5 char<br>";
+    }
 
+    if(strlen($password)>=4)
+    {
+        $validpassword = $password;
+        echo "Password: ".$validpassword."<br>";
+    }
+    else
+    {   
+        echo "Password must be at least 4 characters<br>";               
+    }
 
+    if($email != "")
+    {
+        echo "Email: ".$email."<br>";
+    }
+    else
+    {
+        echo "Invalid Email format<br>";
+    }
+        if($website != "")
+    {
+        echo "Website: ".$website."<br>";
+    }
+    else
+    {
+        echo "Website need to write<br>";
+    }
 
+    if($comment != "")
+    {
+        echo "Comment: ".$comment."<br>";
+    }
+    else
+    {
+        echo "Comment is empty<br>";
+    }
+
+    if(!empty($gender))
+    {
+        echo "Gender: ".$gender."<br>";
+    }
+    else
+    {
+        echo "Gender is required to select<br>";
+    }
+    
+if(!empty($name) && strlen($name)>=5 && strlen($password)>=4 
+        && $email != "" && $website != "" && $comment != "" && !empty($gender))
+    {
+        $_SESSION["name"] = $name;
+        setcookie("name", $name, time()+3600, "/");
+        echo "Login Successful<br>";
+    }
+    else
+    {
+        echo "Please fill all fields correctly!<br>";
+    }
+}
+
+if(isset($_SESSION["name"]))
+{
+    echo "Welcome ".$_SESSION["name"];
+}
+elseif(isset($_COOKIE["name"]))
+{
+    echo "Welcome ".$_COOKIE["name"];
+}
+else
+{
+    echo "Please log in again!";
+}
 
 ?>
---
+++ FINAL Term/T_SessionCokkie/View/form.php
@@ -0,0 +1,59 @@
+<?php
+include "../Controller/Registratiovalidation.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Registration Log In Form</title>
+    </head>
+
+    <body>
+        <form method ="post" action="">
+            <table>
+                <tr>
+                     <td><p style = 'color: red '> * Required Field </p></td><br>
+                </tr>
+                <tr>
+                    <td> <label for ="UserName"> User Name: </label></td>
+                    <td> <input type ="text" id = "name" name = "name"> <?php echo $name ?></td>
+                    <td> <p style = 'color: red'>*</p> </td>
+                
+                </tr>
+                <tr>
+                    <td> <label for ="password"> Password: </label></td>
+                    <td> <input type = "password" id="pass" name ="password"><?php echo $password ?></td>
+                </tr>
+                <tr>
+                   <td><label for="email">E-mail:</label></td>
+                   <td><input type="email" id="email" name="email"><?php echo $email; ?></td>
+                   <td><p style="color:red;">*</p></td>
+                </tr>   
+                <tr>
+                    <td>Website:</td>
+                    <td><input type="text" name="website"> <?php echo $website; ?></td>
+                </tr>
+                <tr>
+                    <td>Comment:</td>
+                    <td><textarea name="comment"></textarea> <?php echo $comment; ?> </td>
+                </tr>
+
+                <tr>
+                    <td>Gender:</td>
+                    <td>
+                        <input type="radio" name="gender" value="Female">Female
+                        <input type="radio" name="gender" value="Male">Male
+                        <input type="radio" name="gender" value="Other">Other
+                        <?php echo $gender; ?>
+                    </td>
+                    <td style="color:red">*</td>
+                </tr>
+
+                <tr>
+                    <td><input type = "submit" id = "submit" name = "submit" value="Submit"> </td>
+                </tr>
+                
+            </table>
+        </form>
+    </body>
+</html>
